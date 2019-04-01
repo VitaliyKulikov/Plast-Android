ede6eb86209b52e0a4fd3d087c3e386ebc910b7f
Roman Khrupa <hruparoman@gmail.com>
2019-04-01 23:45:18 +0300

    updated some extensions

--- app/src/main/java/com/bbook/app/toolbox/extensions/KtUtils.kt
+++ app/src/main/java/com/bbook/app/toolbox/extensions/KtUtils.kt
@@ -7,3 +7,11 @@ fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R
 fun <T1 : Any, T2 : Any, T3 : Any, R : Any> safeLet(p1: T1?, p2: T2?, p3: T3?, block: (T1, T2, T3) -> R?): R? {
     return if (p1 != null && p2 != null && p3 != null) block(p1, p2, p3) else null
 }
+
+/**
+ * Implementation of lazy that is not thread safe. Useful when you know what thread you will be
+ * executing on and are not worried about synchronization.
+ */
+fun <T> lazyFast(operation: () -> T): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) {
+    operation()
+}
--- app/src/main/java/com/bbook/app/toolbox/extensions/SoftKeyboardUtils.kt
+++ app/src/main/java/com/bbook/app/toolbox/extensions/SoftKeyboardUtils.kt
@@ -12,20 +12,27 @@ fun Activity.showKeyboard(target: View) {
     keyboard.showSoftInput(target, 0)
 }
 
+
 fun Activity.hideKeyboard() {
     val view = currentFocus
     hideKeyboard(view)
 }
 
 fun Fragment.hideKeyboard() {
-    activity?.hideKeyboard(null)
+    safeLet(context, view) { context, view ->
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    } ?: let {
+        activity?.hideKeyboard()
+    }
 }
 
 fun Activity.hideKeyboard(target: View?) {
-    val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
     target?.let {
-        keyboard.hideSoftInputFromWindow(target.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
+        target.postDelayed(Runnable {
+            val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            keyboard.hideSoftInputFromWindow(target.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
+        }, 2000)
     } ?: let {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
     }
