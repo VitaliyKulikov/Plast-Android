1e84a90cc76acaf249471efd1fca5998ab8a1fdc
Roman Korostenskyi <romankorostensky@gmail.com>
2019-04-06 22:13:00 +0300

    Add login feature

--- app/src/main/java/com/plast/app/features/login/fragments/LoginFragment.kt
+++ app/src/main/java/com/plast/app/features/login/fragments/LoginFragment.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import com.plast.app.AppViewModelsFactory
 import com.plast.app.R
 import com.plast.app.baseui.BaseFragment
-import com.plast.app.toolbox.extensions.toastSh
 import com.plast.app.features.login.viewmodel.LoginViewModel
-import com.plast.app.features.main.MainActivity
-import com.plast.app.features.signup.SignUpActivity
 import kotlinx.android.synthetic.main.fragment_login.*
 import javax.inject.Inject
 
@@ -18,6 +20,14 @@ class LoginFragment : BaseFragment(),View.OnClickListener {
     @Inject
     lateinit var vmFactory: AppViewModelsFactory
     private lateinit var viewModel: LoginViewModel
+
+    private val RC_SIGN_IN = 1
+
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var rootView: View
 
     companion object {
         val TAG = LoginFragment::class.java.simpleName
@@ -27,6 +37,10 @@ class LoginFragment : BaseFragment(),View.OnClickListener {
     override fun layoutId(): Int = R.layout.fragment_login
 
     override fun onViewReady(inflatedView: View, args: Bundle?) {
+
+        rootView = inflatedView
+        firebaseAuth = FirebaseAuth.getInstance()
+
         setListeners()
     }
 
@@ -37,34 +51,34 @@ class LoginFragment : BaseFragment(),View.OnClickListener {
     }
 
     override fun setListeners() {
-        forget_password.setOnClickListener(this)
-        sign_in.setOnClickListener(this)
-        sign_in_with_facebook.setOnClickListener(this)
-        sign_up.setOnClickListener(this)
-        skip.setOnClickListener(this)
-
+        signInButton.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when(v){
-            forget_password->{
-                toastSh("forget password")
-            }
-            sign_in->{
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.key))
+            .requestEmail()
+            .build()
 
-            }
-            sign_in_with_facebook ->{
+        googleSignInClient = GoogleSignIn.getClient(rootView.context, gso)
 
-            }
-            sign_up->{
-                startActivity(Intent(activity, SignUpActivity::class.java))
-            }
-            skip ->{
-                startActivity(Intent(activity, MainActivity::class.java))
-                activity?.finish()
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                viewModel.signInViaGoogle(account)
+            } catch (e: ApiException) {
+                e.printStackTrace()
             }
         }
     }
-
 
 }
