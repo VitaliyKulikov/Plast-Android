3c681394ee2cc3979d4870badb984bd019842635
Pavlo Kuchirka <ku4irka@gmail.com>
2019-04-07 17:38:17 +0300

    card pager update resources

--- app/src/main/java/com/plast/app/repositories/CardRepository.kt
+++ app/src/main/java/com/plast/app/repositories/CardRepository.kt
@@ -14,19 +14,19 @@ class CardRepository {
 
     private fun initCardList() {
         val listOfCard = arrayListOf<CardEntity>()
-        listOfCard.add(CardEntity(0, 10, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(1, 20, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(2, 30, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(3, 40, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(4, 50, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(5, 60, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(6, 70, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(7, 80, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(8, 90, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(9, 100, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(10, 110, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(11, 120, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
-        listOfCard.add(CardEntity(12, 130, R.drawable.ill_step_one, R.string.title_text, R.string.description_text))
+        listOfCard.add(CardEntity(0, 10, R.drawable.ill_step_one, R.string.title_one, R.string.description_one))
+        listOfCard.add(CardEntity(1, 20, R.drawable.ill_step_two, R.string.title_two, R.string.description_two))
+        listOfCard.add(CardEntity(2, 30, R.drawable.ill_step_three, R.string.title_three, R.string.description_three))
+        listOfCard.add(CardEntity(3, 40, R.drawable.ill_step_four, R.string.title_four, R.string.description_four))
+        listOfCard.add(CardEntity(4, 50, R.drawable.ill_step_five, R.string.title_five, R.string.description_five))
+        listOfCard.add(CardEntity(5, 60, R.drawable.ill_step_six, R.string.title_six, R.string.description_six))
+        listOfCard.add(CardEntity(6, 70, R.drawable.ill_step_one, R.string.title_one, R.string.description_one))
+        listOfCard.add(CardEntity(7, 80, R.drawable.ill_step_two, R.string.title_two, R.string.description_two))
+        listOfCard.add(CardEntity(8, 90, R.drawable.ill_step_three, R.string.title_three, R.string.description_three))
+        listOfCard.add(CardEntity(9, 100, R.drawable.ill_step_four, R.string.title_four, R.string.description_four))
+        listOfCard.add(CardEntity(10, 110, R.drawable.ill_step_five, R.string.title_five, R.string.description_five))
+        listOfCard.add(CardEntity(11, 120, R.drawable.ill_step_six, R.string.title_six, R.string.description_six))
+        listOfCard.add(CardEntity(12, 130, R.drawable.ill_step_one, R.string.title_one, R.string.description_one))
         cardList = listOfCard
     }
 }
